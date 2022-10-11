7ab30f7ea4b33731f8fa14a064dc008cfc6c9e4d
kuand <kuand3203@gmail.com>
2022-10-11 19:28:07 +0300

    add CSRF_TRUSTED_ORIGINS

--- app/big_site/settings.py
+++ app/big_site/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", 'nSAVJHASBVFDHJL23R3434BR45234114R')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split('||')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split('||')
 
 
 # Application definition
