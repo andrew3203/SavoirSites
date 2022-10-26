1ca651430c47504da903d8a700418ff9e9501cbb
kuand <kuand3203@gmail.com>
2022-10-27 01:34:34 +0300

    add mail

--- app/abig/settings.py
+++ app/abig/settings.py
@@ -157,3 +157,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
+
+
+
+# Bottom of the file
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+RECIPIENT_ADDRESS = os.environ.get('RECIPIENT_ADDRESS').strip().split(',')
